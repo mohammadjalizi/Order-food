f076012296473ece95497183b1cfa71c300b9199
mohammadjalizi <mjalizi1376@gmail.com>
2023-01-10 15:55:09 +0330

    addproject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import { useState } from "react";
-import Header from "./Header";
-import Compennet4 from "./Compennet4";
-import Data from "./Data";
-import Loading from "./Loading";
+
+ import Navbar from "./navbar/Navbar";
+import Proudoct from "./proudoct.jsx/proudoct";
+import Footer from "./footer/Footer";
 
 const App=()=> {
-const[loading,setloading]=useState(true);
 
   return (
    <>
-<Header/>
-
-
-<button onClick={()=>setloading (!loading)} >chanhloading</button>
-<hr/>
-{
-loading ? <loading/>  :  <Data/>
-
-}
+<Navbar/>
+<Proudoct/>
+<Footer/>
    </>
 
   );
---
+++ src/proudoct.jsx/proudoct.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Proudoct() {
+  return (
+    <div>proudoct</div>
+  )
+}
+
+export default Proudoct
